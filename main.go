4c947e91d7d2b6ab544b16d87041f274237d64d7
Araden14 <arnaud-danselme@hotmail.com>
2025-11-03 15:14:58 +0100

    global var to store contacts, create function to render menu, wip contact creation

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func initialModel() []string {
+var contacts map[string]string
+
+func printItems() []string {
 	choices := []string{
 		"1) Add contact",
 		"2) List contacts",
@@ -12,14 +16,40 @@ func initialModel() []string {
 		"4) Update contact",
 		"5) Quit",
 	}
-	return choices
-}
-func main() {
-	for i := 0; i < len(initialModel()); i++ {
-		fmt.Print(initialModel()[i] + "\n")
+	for i := 0; i < len(choices); i++ {
+		fmt.Print(choices[i] + "\n")
 
 	}
+	return choices
+}
+
+func miniCRM() {
+	printItems()
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	// Step 1 : if input not integer between 1 and 5, ask again
+	for {
+		if input == "1\n" || input == "2\n" || input == "3\n" || input == "4\n" || input == "5\n" {
+			break
+		}
+		fmt.Print("Invalid input. Please enter a number between 1 and 5: ")
+		input, _ = reader.ReadString('\n')
+	}
+
+	if input == "1\n" {
+		// prefix string "Enter contact name: "
+		fmt.Print("Enter contact name: ")
+		input, _ := reader.ReadString('\n')
+		fmt.Print("name :", input)
+		miniCRM()
+	}
+	// Function to make program persistent
 	for {
 
 	}
+}
+
+func main() {
+	miniCRM()
 }
